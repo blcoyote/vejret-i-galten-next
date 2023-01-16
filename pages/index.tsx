a9c849cb430f74d11a9ac4d45d0d450115fb8eaa
Brian LEach Christensen <blc@elcoyote.dk>
2023-01-16 15:35:00 +0100

    update #1

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,11 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import database from '../utils/db';
+import { getDatabase, ref, onValue } from 'firebase/database';
+import { useEffect } from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
