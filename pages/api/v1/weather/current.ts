741c910f1815abfd6f24027e09852779f0db845a
Brian LEach Christensen <blc@elcoyote.dk>
2023-03-02 10:56:48 +0100

    abstracted firebase from route

--- pages/api/v1/weather/current.ts
+++ pages/api/v1/weather/current.ts
@@ -1,33 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import { getDatabase, query, ref, orderByChild, limitToFirst, limitToLast, onValue, off } from 'firebase/database';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { getCurrentWeather } from '../../../../utils/db/getWeather';
 import WeatherObservation from './../../../../models/WeatherObservation';
-import { database, auth } from './../../../../utils/db';
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<WeatherObservation | { error: string }>
+  res: NextApiResponse<WeatherObservation | { error: string } | undefined>
 ) {
   const { id } = req.query;
-  if (req.method !== 'GET') {
-    res.status(405);
-    res.end();
-  }
-  try {
-    const weather = query(ref(database, `weather`), orderByChild('dateepoch'), limitToLast(1));
 
-    onValue(weather, (snapshot) => {
-      if (snapshot.exists()) {
-        const observation = Object.values(snapshot.val())[0] as WeatherObservation;
+  if (req.method !== 'GET') {
+    res.status(405).setHeader('Allow', 'GET').send(undefined);
+  }
+
+  await getCurrentWeather()
+    .then((observation) => {
+      if (observation) {
         res.status(200).json(observation);
       } else {
         res.status(404).json({ error: 'No weather observations found' });
       }
-      off(weather);
+    })
+    .catch((error) => {
+      res.status(500).send(undefined);
     });
-  } catch {
-    res.status(500);
-    res.end();
-  }
 }
---
+++ utils/db/getWeather.ts
@@ -0,0 +1,23 @@
+import { database } from '.';
+import { query, ref, orderByChild, limitToLast, get } from 'firebase/database';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import WeatherObservation from './../../models/WeatherObservation';
+
+const dbRef = ref(database, 'weather');
+
+export async function getCurrentWeather(): Promise<WeatherObservation | undefined> {
+  const weather = query(dbRef, orderByChild('dateepoch'), limitToLast(1));
+
+  return get(weather)
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        const observation = Object.values(snapshot.val())[0] as WeatherObservation;
+        return observation;
+      } else {
+        return undefined;
+      }
+    })
+    .catch((error) => {
+      throw new Error(error);
+    });
+}
