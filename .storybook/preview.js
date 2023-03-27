4852a0a69394319141f798465318727eee1ee99e
Brian LEach Christensen <blc@elcoyote.dk>
2023-03-27 13:57:33 +0200

    added working storybook and mock weather data

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,29 @@
+import { lightTheme } from '../theme';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { store } from '../store/store';
+import { Provider } from 'react-redux';
+import da from '../lang/da.json';
+import { IntlProvider } from 'react-intl';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <IntlProvider locale={'da'} messages={da} defaultLocale={'da'}>
+      <Provider store={store}>
+        <ThemeProvider theme={lightTheme}>
+          <CssBaseline />
+          <Story />
+        </ThemeProvider>
+      </Provider>
+    </IntlProvider>
+  ),
+];
